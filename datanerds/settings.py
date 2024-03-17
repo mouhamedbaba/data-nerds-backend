7ce6fc8046abbf372442fb5a9495a0a8deb87c4c
mouhamedbaba <lamottelymouhamed@gmail.com>
2024-03-17 12:59:50 +0000

    mysql

--- datanerds/settings.py
+++ datanerds/settings.py
@@ -73,10 +73,22 @@ WSGI_APPLICATION = 'datanerds.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'data_nerds_v1',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost', # ou l'adresse de votre serveur MySQL
+        'PORT': '3306', # le port par défaut de MySQL
     }
 }
 
