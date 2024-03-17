80e0399cd269f6e60092e4c6f6f7d0c1157b6ade
mouhamedbaba <lamottelymouhamed@gmail.com>
2024-03-17 14:14:42 +0000

    erfgfb

--- datanerds/settings.py
+++ datanerds/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = "django-insecure-fc@pbhw%4+(!@lzvkv9nrb2+(u%1jbid)=lw@cf2^fr^_74g0z"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
